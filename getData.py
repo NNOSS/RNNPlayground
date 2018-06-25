f5a3879fbabca4c00c2b3e8b70a5eea6c92e6441
CleanestMink126 <gns126@gmail.com>
2018-06-25 16:24:31 -0400

    wrote load functions

---
+++ getData.py
@@ -0,0 +1,43 @@
+import numpy as np
+import string
+import re
+
+def getNamestxt(filename):
+    '''build database from text file where it only takes first word from
+    each line'''
+    with open(filename,"r") as f:
+        wordlist = [r.lower().replace("\n",'').split('\t')[0] for r in f]
+    return wordlist
+
+def getNamescsv(filename):
+    '''build database from csv where it only takes the first word ff first column'''
+    wordlist = []
+    with open(filename, 'r') as csvfile:
+        reader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+        for row in reader:
+            f = str(row[0])
+            # print(type(f))
+            f = re.split("[, '\-!?:.$12368&]+", f)
+            # print(f)
+
+            if len(f[0]) > 2: wordlist.append(f[0].lower())
+            # for i in f[0].lower():
+            #     if i not in string.ascii_lowercase:
+            #         print(i)
+    return wordlists
+
+def get_batch_generator(filename, batch_size, length):
+    wordlist = getNamestxt(filename)
+    i = 0
+    while i + batch_size < len(wordlist):
+        batch = np.zeros([length, batch_size], dtype=np.int8)
+        for j, word in enumerate(wordlist[i:i+batch_size]):
+            indices = [string.ascii_lowercase.index(letter) + 1 for letter in word]
+            if len(indices) > length:
+                indices = indices[:length]
+            batch[np.arange(len(indices)), j] = indices
+        yield batch
+
+if __name__ == "__main__":
+    bg = get_batch_generator('malenames.txt', 10, 10)
+    a = next(bg)
--- main.py
+++ main.py
@@ -1,20 +1,49 @@
 import tensorflow as tf
 import RNNCell
+import getData
+INPUT_SIZE = 26
+HIDDEN_SIZE = 256
+BATCH_SIZE = 100
+FILENAME = 'malenames.txt'
 
 
-def train(output):
-
+def train(train, y):
+    bg = getData.get_batch_generator(FILENAME, 10, 10)
 
 
 def define_train(num_outputs):
     GRNN = RNNCell.GRNNCell(INPUT_SIZE, HIDDEN_SIZE)
-    y = tf.placeholder(shape=(num_outputs, BATCH_SIZE, INPUT_SIZE))
+    y = tf.placeholder(shape=(num_outputs, BATCH_SIZE, 1))
+    y_one_hot = tf.one_hot(y, INPUT_SIZE, axis = 2)
     hidden = tf.zeros(BATCH_SIZE, HIDDEN_SIZE)
     x = tf.zeros(BATCH_SIZE, INPUT_SIZE)
     loss= tf.constant(0)
     for i in range(num_outputs):
         output, hidden = GRNN(x, hidden)
-        x = tf.gather(y, tf.constant(i), axis = 0)
+        x = tf.gather(y_one_hot, tf.constant(i), axis = 0)
         loss = loss + tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=x, logits = output))
     loss = loss / num_outputs
     train = tf.train.AdamOptimizer(LEARNING_RATE).minimize(loss)
+    return y, train
+
+def define_test(num_outputs, x = None):
+    GRNN = RNNCell.GRNNCell(INPUT_SIZE, HIDDEN_SIZE)
+    hidden = tf.zeros(BATCH_SIZE, HIDDEN_SIZE)
+    if x is None:
+        x = tf.zeros(BATCH_SIZE, INPUT_SIZE)
+    loss= tf.constant(0)
+    final_output = None
+    for i in range(num_outputs):
+        output, hidden = GRNN(x, hidden)
+        softmax = tf.nn.softmax(output, axis = 1)
+        choices = tf.multinomial(softmax, 1)
+        if final_output is None:
+            final_output = choices
+        else:
+            final_output = tf.concat([final_output, choices], axis = 1)
+        x = tf.one_hot(choices, INPUT_SIZE)
+    return final_output
+
+if __name__ == "__main__":
+
+    define_train(10)
